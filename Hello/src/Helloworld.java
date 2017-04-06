ef8bdaf9ce8c0612fb485cf7809d64c467166680
hayang_winston <bba3433@naver.com>
2017-04-06 11:00:20 +0900

    Update Helloworld.java
    PushPrint.java modified on GitHub

--- Hello/src/Helloworld.java
+++ Hello/src/Helloworld.java
@@ -3,10 +3,9 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World !! ~!~!~!~!");
-		System.out.println("Hello his world");
-		System.out.println("Hello your world");
-		System.out.println("Hello her world");
+		System.out.println("Add Java file");
+		System.out.println("Hello...");
+		
 
 	}
 
